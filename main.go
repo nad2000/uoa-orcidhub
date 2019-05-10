5a69634a2ef6d93ea63da4001d0cf823d2d42c06
Rad Cirskis <nad2000@gmail.com>
2019-05-10 16:45:36 +1200

    assets

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
-//go:generate swagger generate client -f assets/uoa-employment-v1.json
-//go:generate swagger generate client -f assets/orcidhub-api-v0.1-spec.json  -A orcidhub
+//go:generate sh -c "swagger generate client -f assets/uoa-employment-v1.json"
+//go:generate sh -c "swagger generate client -f assets/orcidhub-api-v0.1-spec.json -A orcidhub"
 
 package main
 
