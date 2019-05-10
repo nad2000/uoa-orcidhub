770256ac42fdfaeab3efaac156b2f1e1bd6d2c2c
Rad Cirskis <nad2000@gmail.com>
2019-05-10 17:43:01 +1200

    server  generagion and add dependencies

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 //go:generate swagger generate client -f ./assets/uoa-employment-v1.json
 //go:generate swagger generate client -f ./assets/orcidhub-api-v0.1-spec.json -A orcidhub
+//go:generate swagger generate server -A uoa-orcidhub
 
 package main
 
