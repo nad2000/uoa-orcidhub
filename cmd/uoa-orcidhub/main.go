6f10566041d2648ecf0c2b0694be985982044324
Rad Cirskis <nad2000@gmail.com>
2019-06-23 21:20:29 +1200

    working TSL server ...

--- cmd/uoa-orcidhub/main.go
+++ cmd/uoa-orcidhub/main.go
@@ -1,35 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"net/http"
-	"time"
+
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/nad2000/uoa-orcidhub/gen/restapi"
+	"github.com/nad2000/uoa-orcidhub/gen/restapi/operations"
 
 	log "github.com/sirupsen/logrus"
 )
 
-type Message struct {
-	Subject string `json:"subject"`
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
-func echo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `{"now": %q}`, time.Now().Format(time.RFC3339))
-}
+var portFlag = flag.Int("port", 3000, "Port to run this service on")
 
 func handleUpdate(w http.ResponseWriter, r *http.Request) {
 }
 
-func handleRequests() {
-	http.HandleFunc("/echo", echo)
-	// http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":8081", nil))
-}
-
 func main() {
-	handleRequests()
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.WithError(err).Fatalf("Failed to open the spec: %q", restapi.SwaggerJSON)
+	}
+	api := operations.NewOrcidhubIntegrationAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+	flag.Parse()
+	server.Port = *portFlag
+	defer server.Shutdown()
+	// TODO: Set Handle
+	api.GetPingHandler = operations.GetPingHandlerFunc(
+		func(params operations.GetPingParams) middleware.Responder {
+			return operations.NewGetPingOK().WithPayload("hello!")
+		})
+
+	if err := server.Serve(); err != nil {
+		log.WithError(err).Fatal("Failed to start the servers.")
+	}
 }
--- main.go
+++ main.go
@@ -4,8 +4,48 @@
 
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/nad2000/uoa-orcidhub/server"
+)
+
+const message = "..."
+
+func getenv(key, defaultValue string) {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		value = defaultValue
+	}
+	return value
+}
+
+var (
+	CertFile    = os.Getenv("CERT_FILE")
+	KeyFile     = os.Getenv("KEY_FILE")
+	ServiceAddr = os.Getenv("SERVICE_ADDR")
+)
 
 func main() {
-	fmt.Println("vim-go")
+
+	if CertFile == "" {
+		CertFile 
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// microoptimization:
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(message))
+	})
+
+	log.Infoln("Starting the server...")
+	// err := http.ListenAndServe(":8080", mux) // Invokes the built-in server
+
+	srv := server.New(mux, ServiceAddr)
+	err := srv.ListenAndServeTLS(CertFile, KeyFile)
+	if err != nil {
+		log.WithError(err).Errorln("Failed to start the server.")
+	}
 }
